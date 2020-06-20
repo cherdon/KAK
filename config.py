3c78223cceced4b4859ddcfe3937f4091d553f93
cherdon <liewcherdon@gmail.com>
2020-06-20 10:21:24 +0800

    Store and Profile page done + Temporary store db + Adding more images

--- config.py
+++ config.py
@@ -5,6 +5,10 @@ interface_file = os.path.join(os.path.dirname(__file__), 'interface.json')
 with open(interface_file, 'r') as outfile:
     interface = json.load(outfile)
 
+store_file = os.path.join(os.path.dirname(__file__), 'database', 'stores.json')
+with open(store_file, 'r') as outfile:
+    store_db = json.load(outfile)
+
 
 class Config(object):
     # Flask Framework
@@ -17,3 +21,4 @@ class Config(object):
     TEMPLATE_PATH = os.path.join(os.path.dirname(__file__), 'templates')
     STATIC_PATH = os.path.join(os.path.dirname(__file__), 'static')
     INTERFACE = interface
+    STOREDB = store_db
